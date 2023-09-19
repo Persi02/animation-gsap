865cb72067dd41d54b788ba3c9fb76ce6203f390
Persi <andriamananandroh@gmail.com>
2023-09-19 11:08:56 +0300

    add hello in index.html

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 import { gsap } from 'gsap'
 import './style.css'
 const p = document.querySelector(".test");
-const test = "HELLO _WORLD"
-const arr = test.split('');
+
+const arr = p.textContent.split('');
+p.textContent = ''
 
 for (const letter of arr) {
   const span = document.createElement("span");
   span.textContent = letter;
   p.appendChild(span)
 }
+
+
+
 
 gsap.from("span", {
   opacity: 0, stagger: 0.2, duration: 3, repeat: -1
