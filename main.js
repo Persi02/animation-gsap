ae8a05cc431cb15e06752d5a4ade4ac7bee0409e
Persi <andriamananandroh@gmail.com>
2023-09-19 11:39:10 +0300

    creat function splitText

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 import { gsap } from 'gsap'
 import './style.css'
+import { Timeline } from 'gsap/gsap-core';
 const p = document.querySelector(".test");
 
-const arr = p.textContent.split('');
-p.textContent = ''
 
-for (const letter of arr) {
-  const span = document.createElement("span");
-  span.textContent = letter;
-  p.appendChild(span)
-}
+const txtWelcome = document.querySelector(".welcome");
 
+//function split text
+const splitText = (element) => {
+  const arr = element.textContent.split('');
+  element.textContent = '';
+  for (const letter of arr) {
+    const span = document.createElement("span");
+    span.textContent = letter;
+    element.appendChild(span)
+  }
+};
 
+splitText(p);
+splitText(txtWelcome);
 
+const tl = gsap.timeline();
 
-gsap.from("span", {
-  opacity: 0, stagger: 0.2, duration: 3, repeat: -1
+tl.from(".test span", {
+  opacity: 0, stagger: 0.1
 })
+  .from(".welcome span", {
+    opacity: 0, stagger: 0.1
+  })
 
 
