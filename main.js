a1fda3f886563d9c22e7ef4f2b0ff599858c5b1d
Persi <andriamananandroh@gmail.com>
2023-09-19 12:32:08 +0300

    rectif anim

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
 import { gsap } from 'gsap'
 import './style.css'
-import { Timeline } from 'gsap/gsap-core';
-const p = document.querySelector(".test");
+const p = document.querySelectorAll("p");
 
 
-const txtWelcome = document.querySelector(".welcome");
 
 //function split text
-const splitText = (element) => {
-  const arr = element.textContent.split('');
-  element.textContent = '';
-  for (const letter of arr) {
-    const span = document.createElement("span");
-    span.textContent = letter;
-    element.appendChild(span)
+const splitText = (elements) => {
+  for (const element of elements) {
+    const arr = element.textContent.split('');
+    element.textContent = '';
+    for (const letter of arr) {
+      const span = document.createElement("span");
+      span.textContent = letter;
+      element.appendChild(span)
+    }
   }
+
+
 };
 
 splitText(p);
-splitText(txtWelcome);
 
-const tl = gsap.timeline();
-
-tl.from(".test span", {
+gsap.from("p span", {
   opacity: 0, stagger: 0.1
 })
-  .from(".welcome span", {
-    opacity: 0, stagger: 0.1
-  })
 
 
